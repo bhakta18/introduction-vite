9abc04a05f2e8430bbd8612059d1a9fd3ee2610d
Bhakta <bhaktabhoi.in@gmail.com>
2024-07-30 02:48:32 +0530

    first commit

---
+++ src/app.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import Todo from '/Todo';
+
+const App = () => {
+  return (
+    <div className="App">
+      <h1>My Vite App</h1>
+      <Todo />
+    </div>
+  );
+};
+
+export default App;
---
+++ src/todo.js
@@ -0,0 +1,42 @@
+import React, { useState } from 'react';
+import './Todo.css'; // Optional: for adding some styles
+
+const Todo = () => {
+  const [todos, setTodos] = useState([]);
+  const [input, setInput] = useState('');
+
+  const addTodo = () => {
+    if (input.trim()) {
+      setTodos([...todos, input]);
+      setInput('');
+    }
+  };
+
+  const deleteTodo = (index) => {
+    const newTodos = todos.filter((_, i) => i !== index);
+    setTodos(newTodos);
+  };
+
+  return (
+    <div className="todo-app">
+      <h2>Todo List</h2>
+      <input
+        type="text"
+        value={input}
+        onChange={(e) => setInput(e.target.value)}
+        placeholder="Add a new todo"
+      />
+      <button onClick={addTodo}>Add Todo</button>
+      <ul>
+        {todos.map((todo, index) => (
+          <li key={index}>
+            {todo}
+            <button onClick={() => deleteTodo(index)}>Delete</button>
+          </li>
+        ))}
+      </ul>
+    </div>
+  );
+};
+
+export default Todo;
